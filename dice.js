3cb8a012968d2751908b8d4e50c13ca8779241a6
Nelson C <nelson.cavalheiro@outlook.com>
2018-02-01 19:21:01 +0100

    basic code for the mrdice bot

---
+++ dice.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const token = 't0k3n_g0es_h3r3_mat3';
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+  client.user.setGame(`try /howto`);
+});
+
+
+function rand(x) {
+    return Math.floor(Math.random() * x + 1);
+}
+
+client.on('message', msg => {
+    input = msg.content.toLowerCase()
+    //Prints command list
+    if (msg.content === '/howto') {
+        msg.reply(`
+        commands :
+            /r x - roll a dice from 1 to x
+            /loot - randomise a chest or a reward
+            /heal - heal a certain amount of HP
+            /crazy msg - change "msg" with a word and *surprise*
+        `);
+    }
+    switch(input) {
+        //Randomises loot!     
+        case '/loot':
+            loot = ['weapon!','armor!','accessory!','item!','edible!','key item!','money!','lot of money!'];
+            msg.reply(loot[rand(loot.length)]);
+            break;
+        case '/heal':
+            msg.reply('+' + rand(10) + 'PV!');
+            break;
+//Rolls a dice  
+    if (msg.content.slice(0, 3) == '/r ') {
+        const dice = Number(msg.content.slice(3));
+        msg.channel.send(rand(dice));
+    }
+//Returns a "tts" message (read lowdly) and delete the command message (to make people crazy, they won't know who is talking)    
+    if (msg.content.slice(0, 5) === '/crazy ') {
+        msg.delete();
+        msg.channel.send(msg.content.slice(5), {tts: true});
+    }
+    if (msg.content.slice(0, 1) == '!') {
+        msg.channel.send(`use /howto to get a command list or just ask my Master! Also, try using the same command but with a slash instead! `)
+    }
+
+})
+client.login(token);
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Tamabot by Santartine
+
+## Concept
+
+Bot pour JDR
+
+## Fonctionnalitées
+
+Utilisez la commande /howto pour avoir une liste des fonctionnalités
+
+## Comment ça marche ?
+
+see package.json for more details.
